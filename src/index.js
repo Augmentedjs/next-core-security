bf3da420acf20be25e63d17305f9d2c5a18fafc4
thedocbwarren <jedi.master@augmentedjs.com>
2020-07-09 14:34:51 -0700

    update to 3.x

--- src/index.js
+++ src/index.js
@@ -6,12 +6,4 @@ import AuthenticationFactory from "./authenticationFactory.js";
 import Principal from "./principal.js";
 import Entry from "./entry.js";
 import Context from "./context.js";
-
-module.exports.ClientType = ClientType;
-module.exports.AuthenticationFactory = AuthenticationFactory;
-module.exports.Context = Context;
-module.exports.Entry = Entry;
-module.exports.Principal = Principal;
-module.exports.AbstractSecurityClient = AbstractSecurityClient;
-module.exports.ACLClient = ACLClient;
-module.exports.OAUTH2Client = OAUTH2Client;
+export { ClientType, AuthenticationFactory, Context, Entry, Principal, AbstractSecurityClient, ACLClient, OAUTH2Client };
